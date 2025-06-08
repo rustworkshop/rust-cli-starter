95f9f53bd6e456057b1d1c2de333155a50708bb6
Tim Abell <t@0x5.uk>
2025-06-08 11:36:33 +0100

    Exit upgrade script cleanly if no changes
    Without this "just" rightly complains about the exist codes.

--- scripts/upgrade-crates.sh
+++ scripts/upgrade-crates.sh
@@ -4,5 +4,12 @@ echo "### Upgrading crates..."
 cargo update
 cargo install cargo-edit
 cargo upgrade --incompatible # from cargo-edit
+
+# exit with "no changes" if no diff on .tool-versions
+if [ `git diff -- Cargo.lock Cargo.toml | wc -l` -eq 0 ]; then
+  echo "No new crate version found."
+  exit 0
+fi
+
 cargo test
 git commit -i Cargo.lock -i Cargo.toml -m "cargo update/upgrade"
--- scripts/upgrade-rust.sh
+++ scripts/upgrade-rust.sh
@@ -6,5 +6,12 @@ latest=`asdf list all rust | tail -n 1`
 echo $latest
 asdf install rust $latest
 asdf local rust $latest
+
+# exit with "no changes" if no diff on .tool-versions
+if [ `git diff -- .tool-versions | wc -l` -eq 0 ]; then
+  echo "No new rust version found."
+  exit 0
+fi
+
 cargo test
 git commit -i .tool-versions -m "Upgrade rust to latest"
