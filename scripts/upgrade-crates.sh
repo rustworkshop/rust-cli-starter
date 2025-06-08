c36e3218d69020402d9416e6b852660917041843
Tim Abell <t@0x5.uk>
2025-06-08 12:05:04 +0100

    Commit upgraded cargo workspaces when upgrading crates
    https://doc.rust-lang.org/book/ch14-03-cargo-workspaces.html

--- scripts/upgrade-crates.sh
+++ scripts/upgrade-crates.sh
@@ -5,11 +5,11 @@ cargo update
 cargo install cargo-edit
 cargo upgrade --incompatible # from cargo-edit
 
-# exit with "no changes" if no diff on .tool-versions
-if [ `git diff -- Cargo.lock Cargo.toml | wc -l` -eq 0 ]; then
+# exit with "no changes" if no diff on Cargo files
+if [ `git diff -- "**Cargo.toml" Cargo.lock | wc -l` -eq 0 ]; then
   echo "No new crate version found."
   exit 0
 fi
 
 cargo test
-git commit -i Cargo.lock -i Cargo.toml -m "cargo update/upgrade"
+git commit --include Cargo.lock -i "**Cargo.toml" -m "cargo update/upgrade"
